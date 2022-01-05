bd73fbb62f6f0f9ca42a3d997f6f3c702a708413
Pedro-B-Alves <pedro22carro@gmail.com>
2022-01-04 21:17:13 -0300

    fazendo a model e o controler da API

---
+++ API/SistemaDeCadastro/Migrations/20220104211155_Banco inicial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SistemaDeCadastro.Migrations
+{
+    public partial class Bancoinicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Idade = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Sexo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
---
+++ API/SistemaDeCadastro/Models/Usuarios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaDeCadastro.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime Idade { get; set; }
+        public string Sexo { get; set; }
+    }
+}
